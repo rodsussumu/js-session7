22c031977c9d9358725172d7c0e31669dcfea906
Rodrigo Tanaka <rstanaka@aluscseduca.com.br>
2020-03-20 22:01:28 -0300

    aula 20/03

---
+++ aula3/src/index.js
@@ -0,0 +1,6 @@
+import {nome, sobrenome, idade, soma, Pessoa} from './modulo1'
+
+const p1 = new Pessoa('Rodrigo', 'Tanaka')
+console.log(nome, sobrenome, idade)
+console.log(soma(5, 5));
+console.log(p1);
